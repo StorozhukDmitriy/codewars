e9633f4817a81a0ccec080b8287ffa2b1936ab2b
DmitriyStorozhuk <gelendzhik94@yandex.ru>
2024-10-02 21:32:44 +0300

    ModSolution categorizeNewMember

--- 7kyu/categorizeNewMember.js
+++ 7kyu/categorizeNewMember.js
@@ -11,14 +11,6 @@
 // output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
 
 //Solution
-function openOrSenior(data) {
-  const result = [];
-  for (let i = 0; i < data.length; i++) {
-    if (data[i][0] >= 55 && data[i][1] > 7) {
-      result.push("Senior");
-    } else {
-      result.push("Open");
-    }
+function openOrSenior(data){
+    return data.map(([year, handicap]) => {return  year >= 55 && handicap > 7 ? "Senior" : "Open"});
   }
-  return result;
-}
