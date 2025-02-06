9b7f326ad3952a66aa8d69e6492e85e73c92766a
Dmitriy Storozhuk <gelendzhik94@yandex.ru>
2025-02-06 22:57:46 +0300

    Solution Round up to the next multiple of 5

--- 8kyu/AddLength.js
+++ 8kyu/AddLength.js
@@ -1,5 +1,5 @@
-/*
-What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
+
+/*What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
 
     Example(Input --> Output)
 
@@ -7,8 +7,8 @@ What if we need the length of the words separated by a space to be added at the 
 "you will win" -->["you 3", "will 4", "win 3"]
 Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
 
-    Note: String will have at least one element; words will always be separated by a space.*/
-]
+    Note: String will have at least one element; words will always be separated by a space.*!/
+]*/
 
 //Solution
 function addLength(str) {
